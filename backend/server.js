0ac45d0cb44478410c5bc4a4d99e6fa684ef1adc
Ayush7433 <maheraayush2302@gmail.com>
2026-06-05 23:40:42 +0530

    Defined document routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ import { error } from 'console';
 import errorHandler from './middleware/errorHandler.js'
 
 import authRoutes from './routes/authRoutes.js'
+import documentRoutes from './routes/documentRoutes.js'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -35,6 +36,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 //Routes
 app.use('/api/auth', authRoutes)
+app.use('/api/documents', documentRoutes)
 
 
 app.use(errorHandler);
