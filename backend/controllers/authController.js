789e1ab6e730ce7ab8f2e5de65af18bbf2f1bf8e
Ayush7433 <maheraayush2302@gmail.com>
2026-06-05 18:10:34 +0530

    Created getProfile, updateProfile and changePassword controllers

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -53,7 +53,7 @@ export const register = async (req, res, next) => {
         },
         token,
       },
-      message: "User registered successfully"
+      message: "User registered successfully",
     });
   } catch (error) {
     next(error);
@@ -66,11 +66,10 @@ export const register = async (req, res, next) => {
 
 export const login = async (req, res, next) => {
   try {
-
     const { email, password } = req.body;
 
     //Validate input
-    if(!email || !password){
+    if (!email || !password) {
       return res.status(400).json({
         success: false,
         error: "Please provide email and password",
@@ -79,9 +78,9 @@ export const login = async (req, res, next) => {
     }
 
     //check for user
-    const user = await User.findOne({email}).select("+password");
+    const user = await User.findOne({ email }).select("+password");
 
-    if(!user){
+    if (!user) {
       return res.status(401).json({
         success: false,
         error: "Invalid credentials",
@@ -92,7 +91,7 @@ export const login = async (req, res, next) => {
     //Check password
     const isMatch = await user.matchPassword(password);
 
-    if(!isMatch){
+    if (!isMatch) {
       return res.status(401).json({
         success: false,
         error: "Invalid credentials",
@@ -112,7 +111,7 @@ export const login = async (req, res, next) => {
         profileImage: user.profileImage,
       },
       token,
-      message: "User logged in successfully"
+      message: "User logged in successfully",
     });
   } catch (error) {
     next(error);
@@ -124,6 +123,19 @@ export const login = async (req, res, next) => {
 //@access Private
 export const getProfile = async (req, res, next) => {
   try {
+    const user = await User.findById(req.user._id);
+
+    res.status(200).json({
+      success: true,
+      data: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        profileImage: user.profileImage,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt,
+      },
+    });
   } catch (error) {
     next(error);
   }
@@ -134,6 +146,26 @@ export const getProfile = async (req, res, next) => {
 //@access Private
 export const updateProfile = async (req, res, next) => {
   try {
+    const { username, email, profileImage } = req.body;
+
+    const user = await User.findById(req.user._id);
+
+    if(username) user.username = username;
+    if(email) user.email = email;
+    if(profileImage) user.profileImage = profileImage;
+
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      data: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        profileImage: user.profileImage,
+      },
+      message: "Profile updated successfully",
+    });
   } catch (error) {
     next(error);
   }
@@ -144,6 +176,38 @@ export const updateProfile = async (req, res, next) => {
 //@access Private
 export const changePassword = async (req, res, next) => {
   try {
+    const {currentPassword, newPassword} = req.body;
+
+    if(!currentPassword || !newPassword){
+      return res.status(400).json({
+        success: false,
+        error: "Please provide current and new password",
+        statusCode: 400,
+      });
+    }
+
+    const user = await User.findById(req.user._id).select("+password");
+
+    //check current password
+    const isMatch = await user.matchPassword(currentPassword);
+
+    if(!isMatch){
+      return res.status(401).json({
+        success: false,
+        error: "Current password is incorrect",
+        statusCode: 401,
+      });
+    }
+
+    //Update password
+    user.password = newPassword;
+    await user.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Password changed successfully",
+    });
+
   } catch (error) {
     next(error);
   }
